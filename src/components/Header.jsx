100d92bcbdc9d3bee80f7a20e1711f0c2057d2f8
German G <glezrdg@gmail.com>
2023-03-14 05:43:50 -0400

    finished the navbar appereance

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { BsWhatsapp } from "react-icons/bs";
 import { BsFillArrowUpCircleFill } from "react-icons/bs";
 import { BsFillArrowDownCircleFill } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
 
 function Header({ children }) {
+  // navigate menu
+
+  const menu = useRef(null);
+
+  const openNav = () => {
+    const element = menu.current;
+    element.style.height = "100%";
+    element.style.opacity = "1";
+  };
+  const closeNav = () => {
+    const element = menu.current;
+    element.style.height = "0";
+    element.style.opacity = "0";
+  };
+
+  // scroll change color
+
   const [scrollNav, setScrollNav] = useState(false);
   useEffect(() => {
     window.addEventListener("scroll", changeNav);
@@ -85,12 +103,73 @@ function Header({ children }) {
           scrollNav ? "bg-white shadow-md" : "bg-transparent"
         }`}
       >
+        <div
+          className="overlay-menu flex items-center justify-center text-2xl font-bold text-white"
+          ref={menu}
+        >
+          <a href="javascript:void(0)" class="closebtn" onClick={closeNav}>
+            &times;
+          </a>
+          <div class="flex flex-col justify-evenly items-center h-1/2">
+            <a
+              href="#home"
+              className="hover:text-yellow-300 transition-all "
+              onClick={closeNav}
+            >
+              Home
+            </a>
+            <a
+              href="#about"
+              onClick={closeNav}
+              className="hover:text-yellow-300 transition-all "
+            >
+              About me
+            </a>
+            <a
+              href="#skills"
+              onClick={closeNav}
+              className="hover:text-yellow-300 transition-all "
+            >
+              What I Do
+            </a>
+            <a
+              href="#summary"
+              className="hover:text-yellow-300 transition-all "
+              onClick={closeNav}
+            >
+              Resume
+            </a>
+            <a
+              href="#portfolio"
+              className="hover:text-yellow-300 transition-all "
+              onClick={closeNav}
+            >
+              Portfolio
+            </a>
+            <a
+              href="#contact"
+              className="hover:text-yellow-300 transition-all "
+              onClick={closeNav}
+            >
+              Contact
+            </a>
+            <a
+              href="https://github.com/glezrdg"
+              className="flex items-center web-font font-bold hover:text-yellow-300 transition-all ml-3 mt-5 text-4xl"
+              target="_blank"
+            >
+              GitHub
+              <BsGithub className="ml-3" />
+            </a>
+          </div>
+        </div>
         <nav
           className={`w-[100vw] xl:w-[68vw] xl:m-auto  md:mx-10 lg:mx-10 flex items-center justify-between web-font max-sm:w-full max-sm:px-2 `}
         >
           <h2 className="text-3xl  font-bold">
             <a href="#home">GERMÁN </a>
           </h2>
+
           <div className="flex items-center justify-between text-xl  xl:w-1/4 md:w-2/3 lg:w-1/3 max-sm:w-full  ">
             <a
               className="flex items-center mr-5 font-semibold  text-zinc-800  px-6 py-2 hover:scale-105 transition-all max-sm:text-lg max-sm:font-bold "
@@ -100,7 +179,9 @@ function Header({ children }) {
               <p>+1 (829)-464-9960</p>
             </a>
 
-            <RxHamburgerMenu className="text-4xl max-sm:text-3xl" />
+            <div className="" onClick={openNav}>
+              <RxHamburgerMenu className="text-4xl max-sm:text-3xl hover:scale-110 transition-all" />
+            </div>
           </div>
         </nav>
         <div className="fixed md:hidden lg:hidden w-[5vw] h-[15vh]  xl:flex flex-col justify-between items-center top-[50%] right-0 z-50 cursor-pointer max-sm:hidden">
